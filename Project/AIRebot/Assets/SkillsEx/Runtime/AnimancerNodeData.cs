c19317e76dc56bf49ea2a68d50e65002516726be
billwillman <billwillman@qq.com>
2026-02-19 21:08:16 +0800

    2222

---
+++ Project/AIRebot/Assets/SkillsEx/Runtime/AnimancerNodeData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Animancer;
+
+namespace SkillEditor.Data
+{
+    public class AnimancerNodeData : NodeData
+    {
+        public Animancer.AnimancerTransitionAssetBase Data = null;
+    }
+}

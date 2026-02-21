ab43e84fc5c6bf22d648a6cb073aeae1a73a1302
billwillman <billwillman@qq.com>
2026-02-22 03:03:23 +0800

    111222

--- Project/Excel/luban_excel_export_all.bat
+++ Project/Excel/luban_excel_export_all.bat
@@ -1,11 +1,12 @@
 set WORKSPACE=..
 set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
 set CONF_ROOT=.
+set OUT_DIR=%WORKSPACE%\AIRebot\Assets\Resources\@Config\
 
 dotnet %LUBAN_DLL% ^
     -t all ^
     -d protobuf2-bin ^
     --conf %CONF_ROOT%\luban.conf ^
-    -x outputDataDir=output
+    -x outputDataDir=%OUT_DIR%
 
 pause

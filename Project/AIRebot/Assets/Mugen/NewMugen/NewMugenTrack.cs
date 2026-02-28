2b3a99c8944e6d7b7b2aa2162864d7aed411342f
billwillman <billwillman@qq.com>
2026-03-01 04:45:18 +0800

    1111

--- Project/AIRebot/Assets/Mugen/NewMugen/NewMugenTrack.cs
+++ Project/AIRebot/Assets/Mugen/NewMugen/NewMugenTrack.cs
@@ -7,10 +7,13 @@ using Taco.Timeline;
 
 namespace Taco.Timeline.Mugen
 {
-    [TrackGroup("Character"), ScriptGuid("828a8311b89bab843b655b0181bf41e4")]
-    public class NewMugenImageAnimationTrack : Track
+    [TrackGroup("Character"), ScriptGuid("828a8311b89bab843b655b0181bf41e4"), Color(165, 032, 025)]
+    public class NewMugenImageAnimationTrack : CharacterClip
     {
         [ShowInInspector, OnValueChanged("RebindTimeline")]
         public int actionNo = ImageAnimation._cNoVaildState;
+#if UNITY_EDITOR
+        public NewMugenImageAnimationTrack(Track track, int frame) : base(track, frame) { }
+#endif
     }
 }

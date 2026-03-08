d3e392593e0ea011c795ac1b721bf363d4e326de
billwillman <billwillman@qq.com>
2026-03-09 01:19:50 +0800

    1111

---
+++ Project/AIRebot/Assets/Mugen/Timeline/NewMugenAnimFrameTrack.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Taco.Timeline;
+
+namespace Taco.Timeline.Mugen
+{
+    [TrackGroup("Character"), ScriptGuid("a1066dc4983be8144a7d778751f131b0"), Color(165, 032, 025)]
+    public class NewMugenAnimFrameTrack : CharacterTrack
+    {
+    }
+}

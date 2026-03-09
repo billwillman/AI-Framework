2bb3d2340acf88bd6240a6d28df95acb74738bb7
bestzeng <bestzeng@tencent.com>
2026-03-09 22:03:16 +0800

    22111

---
+++ Project/Extra/Excel_Tools/Luban_Excel_Export/ExcelExportWindow/QExportExcelMainUI.py
@@ -0,0 +1,15 @@
+# This Python file uses the following encoding: utf-8
+from PySide6 import QtCore
+from PySide6 import QtWidgets
+from PyQt6 import uic
+import os
+
+
+class QExportExcelMainUI(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # 加载 .ui 的设计文件
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        ui_file_path = os.path.join(current_dir, "mainwindow.ui")
+        uic.loadui(ui_file_path, self)
+        pass

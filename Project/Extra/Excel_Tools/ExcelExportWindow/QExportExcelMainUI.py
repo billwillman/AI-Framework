e711f96045b0a5acafe37de606e03ee177e41b26
bestzeng <bestzeng@tencent.com>
2026-03-10 21:42:58 +0800

    221111

--- Project/Extra/Excel_Tools/ExcelExportWindow/QExportExcelMainUI.py
+++ Project/Extra/Excel_Tools/ExcelExportWindow/QExportExcelMainUI.py
@@ -1,15 +1,14 @@
 # This Python file uses the following encoding: utf-8
 from PySide6 import QtCore
-from PySide6 import QtWidgets
-from PyQt6 import uic
+from PyQt6 import QtWidgets, uic
 import os
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+ui_file_path = os.path.join(current_dir, "mainwindow.ui")
+Ui_MainWindow, baseClass = uic.loadUiType(ui_file_path)
 
-class QExportExcelMainUI(QtWidgets.QMainWindow):
+class QExportExcelMainUI(baseClass, Ui_MainWindow):
     def __init__(self):
-        super().__init__()
-        # 加载 .ui 的设计文件
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        ui_file_path = os.path.join(current_dir, "mainwindow.ui")
-        uic.loadUi(ui_file_path, self)
+        super(QExportExcelMainUI, self).__init__()
+        self.setupUi(self)
         pass

9d4c9f5097c06c29220a0ca18563aac6b3c58134
billwillman <billwillman@qq.com>
2026-03-11 01:43:35 +0800

    22211

--- Project/Extra/Excel_Tools/ExcelExportWindow/install.bat
+++ Project/Extra/Excel_Tools/ExcelExportWindow/install.bat
@@ -1,3 +1,7 @@
+python.exe -m pip install --upgrade pip
+pip install PyQt6
+pip install PySide6
+
 cd .qtcreator/Python_3_9_13venv/Scripts
 python.exe -m pip install --upgrade pip
 pip install PyQt6

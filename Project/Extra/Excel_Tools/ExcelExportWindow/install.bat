b0a9e13367b9f9c5cfbc4fcec7606cfb9af23e8c
bestzeng <bestzeng@tencent.com>
2026-03-10 21:30:53 +0800

    222111

---
+++ Project/Extra/Excel_Tools/ExcelExportWindow/install.bat
@@ -0,0 +1,16 @@
+cd .qtcreator/Python_3_9_13venv/Scripts
+python.exe -m pip install --upgrade pip
+pip install PyQt6
+pip install PySide6
+cd ..
+cd ..
+cd ..
+
+cd .venv/Scripts
+python.exe -m pip install --upgrade pip
+pip install PyQt6
+pip install PySide6
+cd ..
+cd ..
+
+pause
